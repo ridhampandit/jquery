2eb5cd8c22d430c2ae4b142b0bbaf24df67236a8
Ridham Pandit <126068460+ridhampandit@users.noreply.github.com>
2023-05-24 19:04:26 +0530

    Update save_data.php

--- save_data.php
+++ save_data.php
@@ -17,20 +17,16 @@ $qtys = $_POST['qtys'];
 $mrps = $_POST['mrps'];
 $totals = $_POST['totals'];
 
-$files=$_FILES["file"]["name"];
-            $tmp_name=$_FILES["file"]["tmp_name"];
-            $path="upload/".$files;
-            move_uploaded_file($tmp_name,$path);
 
 // Insert data into the database
-if (!empty($emails) && !empty($myDropdowns) && !empty($qtys) && !empty($mrps) && !empty($files) && count($emails) === count($myDropdowns) && count($emails) === count($qtys) && count($emails) === count($mrps) && count($emails) === count($files)) {
+if (!empty($emails) && !empty($myDropdowns) && !empty($qtys) && !empty($mrps) && count($emails) === count($myDropdowns) && count($emails) === count($qtys) && count($emails) === count($mrps)) {
     $rowCount = count($emails);
 
-    $sql = 'INSERT INTO product (email, myDropdown, qty, mrp, files, total) VALUES (?, ?, ?, ?, ?, ?)';
+    $sql = 'INSERT INTO product (email, myDropdown, qty, mrp, total) VALUES (?, ?, ?, ?, ?)';
     $stmt = $conn->prepare($sql);
 
     for ($i = 0; $i < $rowCount; $i++) {
-        $stmt->bind_param('ssssdd', $emails[$i], $myDropdowns[$i], $qtys[$i], $mrps[$i], $files[$i],$totals[$i]);
+        $stmt->bind_param('sssdd', $emails[$i], $myDropdowns[$i], $qtys[$i], $mrps[$i], $totals[$i]);
         $stmt->execute();
     }
 
